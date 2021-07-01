903f2fcdb388bed50f603b4dd8763f6c54685799
DaFrancc <xenonaught@gmail.com>
2021-07-01 14:26:21 -0500

    something idk

--- logic.js
+++
@@ -1,9 +0,0 @@
-function addDelay() {
-    var elements = document.getElementsByClassName("delayedtransition");
-
-    for(let i = 0; i < elements.length; i++) {
-        elements[i].style.transition = ".15s";
-    }
-}
-
-window.onload = () => {addDelay();}
